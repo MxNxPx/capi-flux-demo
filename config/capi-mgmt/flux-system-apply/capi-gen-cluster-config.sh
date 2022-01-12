d9d46deb77524aa7bff8679b9303d70de799a41b
michael <mxnxpx@gmail.com>
2022-01-12 12:25:27 -0500

    bumping up k8s version from 1.19 to 1.21

--- config/capi-mgmt/flux-system-apply/capi-gen-cluster-config.sh
+++ config/capi-mgmt/flux-system-apply/capi-gen-cluster-config.sh
@@ -2,6 +2,6 @@ clusterctl \
   --config ~/.cluster-api/dev-repository/config.yaml \
   config cluster child \
   -n flux-system \
-  --kubernetes-version 1.19.1 \
+  --kubernetes-version 1.21.1 \
   -i docker:v0.3.8 \
   --worker-machine-count 1 \
